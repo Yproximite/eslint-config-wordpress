28b6836e3cf4e1796b1d401d3f03369cc1e69a9a
Hugo Alliaume <kocal@live.fr>
2018-12-20 16:29:09 +0100

    chore: prefer use `require.resolve()` instead of ESLint auto-behavior (#30)
    To prevent some possible errors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 module.exports = {
   extends: [
-    'standard',
-    'plugin:prettier/recommended',
-    '@yproximite/base',
+    require.resolve('eslint-config-standard'),
+    require.resolve('eslint-config-prettier'),
+    require.resolve('@yproximite/eslint-config-base'),
+  ],
+  plugins: [
+    'prettier',
   ],
   globals: {
     jQuery: true,
     $: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
   },
 };
